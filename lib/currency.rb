ddd7738eb5518fb8fd611a277a7086fe5e60baf7
Reid Paape <wrpaape@gmail.com>
2015-05-21 17:09:30 -0500

    redefined == to object comparison specifications

--- lib/currency.rb
+++ lib/currency.rb
@@ -6,4 +6,8 @@ class Currency
     @code = code
   end
 
+  def ==(obj)
+    self.is_a?(obj.class) && amount.equal?(obj.amount) && code.equal?(obj.code) ? true : false
+  end
+
 end
--- spec/currencyTest.rb
+++ spec/currencyTest.rb
@@ -4,14 +4,23 @@ require_relative '../lib/currency.rb'
 class CurrencyTest < Minitest::Test
 
   def setup
-    amount = 100
-    code = "$"
-    @currency = Currency.new(amount, code)
+    @amount = 100
+    @code = "$"
+    @currency = Currency.new(@amount, @code)
   end
 
   def test_Currency_amount_and_code_exist
     assert(@currency.amount)
     assert(@currency.code)
+  end
+
+  def test_parallel_Currency_objects
+    currency_same = Currency.new(@amount, @code)
+    currency_diff_amount = Currency.new(500, @code)
+    currency_diff_code = Currency.new(@amount, "¥")
+    assert_equal(true, @currency == currency_same)
+    assert_equal(false, @currency == currency_diff_amount)
+    assert_equal(false, @currency == currency_diff_code)
   end
 
 end
