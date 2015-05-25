8c8dadb53ef47a34db01fd6e9b3d1cd79ea389bc
Reid Paape <wrpaape@gmail.com>
2015-05-25 18:23:15 -0500

    currency trader retrofitted to accept an array of CC objects

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -2,39 +2,41 @@ require_relative 'currency.rb'
 require_relative 'currency_converter.rb'
 
 class Currency_trader
-  attr_reader :cc_old, :cc_new, :code_best_trade, :best_bet, :roi
+  attr_reader :cc_objs, :code_best_trades, :best_trades, :roi
 
-  def initialize(curr_table_old, curr_table_new, curr_start)
-    @cc_old = Currency_converter.new(curr_table_old)
-    @cc_new = Currency_converter.new(curr_table_new)
+  def initialize(cc_objs, curr_start)
+    @cc_objs = cc_objs
     @curr_start = curr_start
   end
 
-  def best_bet
-    @code_best_trade = most_appreciating_currency
-    @roi = money_made_from_trade(code_best_trade)
+  def best_trades
+    @code_best_trades = []
+    @curr_current = @curr_start
+    @cc_objs.each_with_index do |cc_obj, ind|
+      break if ind == @cc_objs.size - 1
+      @code_best_trades << most_appreciating_currency(@cc_objs[ind], @cc_objs[ind + 1])
+      @curr_current = cc_obj.convert(@curr_current, @code_best_trades.last)
+    end
+
+    @curr_current = @cc_objs.last.convert(@curr_current, @curr_start.code)
+    @roi = @curr_current - @curr_start
+
     """
-    best trade:     #{code_best_trade.to_s}
+    best trade:     #{code_best_trades[0].to_s}
     starting funds: #{@curr_start.amount.round(2)} #{@curr_start.code}
-    final funds:    #{(@curr_start + @roi).amount.round(2)} #{@curr_start.code}
+    final funds:    #{@roi.amount.round(2)} #{@curr_start.code}
     ROI:            #{@roi.amount.round(2)} #{@curr_start.code}
     """ +
-    "For the span of time provided, trading to #{code_best_trade.to_s} at the start before trading back to " +
+    "For the span of time provided, trading to #{code_best_trades[0].to_s} at the start before trading back to " +
     "#{@curr_start.code} at the end would yield the greatest return on investment"
   end
 
-  def most_appreciating_currency
+  def most_appreciating_currency(cc_obj_old, cc_obj_new)
     #want to trade to currency that appreciates the most (minimizes delta_curr2_rate)
-    delta_curr2_rate = Hash.new
-    @cc_old.curr_table.each do |curr2_code, curr2_rate_old|
-      delta_curr2_rate[curr2_code] = (@cc_new.curr_table[curr2_code] - curr2_rate_old) / curr2_rate_old
+    delta_curr_rate = Hash.new
+    cc_obj_old.curr_table.each do |curr_code, curr_rate_old|
+      delta_curr_rate[curr_code] = (cc_obj_new.curr_table[curr_code] - curr_rate_old) / curr_rate_old
     end
-    delta_curr2_rate.key(delta_curr2_rate.values.min)
-  end
-
-  def money_made_from_trade(code_trade)
-    curr2_old = @cc_old.convert(@curr_start, code_trade)
-    curr_new = @cc_new.convert(curr2_old, @curr_start.code)
-    curr_new - @curr_start
+    delta_curr_rate.key(delta_curr_rate.values.min)
   end
 end
