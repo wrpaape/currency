45767a4be7d9b685e41366ee2b658da4e328663a
Reid Paape <wrpaape@gmail.com>
2015-05-24 20:04:45 -0500

    testing accuracy of currency trader

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -2,7 +2,7 @@ require_relative 'currency.rb'
 require_relative 'currency_converter.rb'
 
 class Currency_trader
-  attr_reader :cc_old, :cc_new, :best_bet
+  attr_reader :code_best_trade
 
   def initialize(curr_table_old, curr_table_new, curr_start)
     @cc_old = Currency_converter.new(curr_table_old)
@@ -24,9 +24,16 @@ class Currency_trader
     # #obtain all potential conversion rates for 2nd trade
     # new_rates_pot =  @cc_new.curr_table.select{ |key| key != code_start && key != code_std }
     #want to trade to currency that appreciates the most (minimizes delta_curr2_rate)
-    code_best_trade = most_appreciating_currency
+    @code_best_trade = most_appreciating_currency
     delta_curr_start = money_made_from_trade(code_best_trade)
-
+    """
+    best trade:     #{code_best_trade.to_s}
+    starting funds: #{@curr_start.amount.to_s} #{@curr_start.code}
+    final funds:    #{(@curr_start + delta_curr_start).amount} #{@curr_start.code}
+    ROI:            #{delta_curr_start.amount} #{@curr_start.code}
+    """ +
+    "For the span of time provided, trading to #{code_best_trade.to_s} at the start before trading back to " +
+    "#{@curr_start.code} at the end would yield the greatest return on investment"
 
   end
 
@@ -41,7 +48,8 @@ class Currency_trader
 
   def money_made_from_trade(code_trade)
     curr2_old = @cc_old.convert(@curr_start, code_trade)
-    curr2_new =
+    curr_new = @cc_new.convert(curr2_old, @curr_start.code)
+    curr_new - @curr_start
   end
 
 
