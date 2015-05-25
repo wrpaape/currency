cf05570e63562572cd443a0c0c5e4969d39f3a48
Reid Paape <wrpaape@gmail.com>
2015-05-24 20:30:58 -0500

    still testing accuracy of currency trader

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -2,7 +2,7 @@ require_relative 'currency.rb'
 require_relative 'currency_converter.rb'
 
 class Currency_trader
-  attr_reader :code_best_trade
+  attr_reader :cc_old, :cc_new, :code_best_trade, :best_bet, :roi
 
   def initialize(curr_table_old, curr_table_new, curr_start)
     @cc_old = Currency_converter.new(curr_table_old)
@@ -11,26 +11,13 @@ class Currency_trader
   end
 
   def best_bet
-    # # conversion rate from current code to table standard
-    # code_start = @curr_start.code.to_sym
-    # code_std = @cc_old.curr_table.key(1.00000)
-    # c1 = @cc_old.curr_table[code_start] ** -1
-    # # obtain all potential conversion rates for 1st trade
-    # c2_pot =  @cc_old.curr_table
-    # puts c2_pot.inspect
-    # puts code_start.inspect
-    # puts code_std.inspect
-    # c2_pot
-    # #obtain all potential conversion rates for 2nd trade
-    # new_rates_pot =  @cc_new.curr_table.select{ |key| key != code_start && key != code_std }
-    #want to trade to currency that appreciates the most (minimizes delta_curr2_rate)
     @code_best_trade = most_appreciating_currency
-    delta_curr_start = money_made_from_trade(code_best_trade)
+    @roi = money_made_from_trade(code_best_trade)
     """
     best trade:     #{code_best_trade.to_s}
-    starting funds: #{@curr_start.amount.to_s} #{@curr_start.code}
-    final funds:    #{(@curr_start + delta_curr_start).amount} #{@curr_start.code}
-    ROI:            #{delta_curr_start.amount} #{@curr_start.code}
+    starting funds: #{@curr_start.amount.round(2)} #{@curr_start.code}
+    final funds:    #{(@curr_start + @roi).amount.round(2)} #{@curr_start.code}
+    ROI:            #{@roi.amount.round(2)} #{@curr_start.code}
     """ +
     "For the span of time provided, trading to #{code_best_trade.to_s} at the start before trading back to " +
     "#{@curr_start.code} at the end would yield the greatest return on investment"
@@ -51,6 +38,4 @@ class Currency_trader
     curr_new = @cc_new.convert(curr2_old, @curr_start.code)
     curr_new - @curr_start
   end
-
-
 end
